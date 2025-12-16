a7710ca19b931fcafe4404b209195b56c380e5b5
h <79205847+regitxx@users.noreply.github.com>
2025-12-16 19:20:07 +0800

    we

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
-import "./reziiix.effects.css";
 
 const plusJakarta = Plus_Jakarta_Sans({
     subsets: ["latin"],
